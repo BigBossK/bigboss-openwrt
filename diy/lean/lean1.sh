4790fe8a8bca9364893777313d1656a535b4ad72
BigBossK <41959549+BigBossK@users.noreply.github.com>
2025-11-27 19:52:52 +0800

    Update lean1.sh

--- diy/lean/lean1.sh
+++ diy/lean/lean1.sh
@@ -5,7 +5,7 @@
 ##添加自己的插件库
 rm -rf ./package/lean/k3screenctrl
                
-sed -i '1i src-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk' feeds.conf.default
-sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '3i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '1i src-git 2305ipk https://github.com/xiangfeidexiaohuo/2305-ipk' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
