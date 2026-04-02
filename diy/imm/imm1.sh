d410ff782858bdc2db6e92c1a58d16cfd124a517
BigBossK <773733679@qq.com>
2026-04-03 01:43:17 +0800

    添加imm

---
+++ diy/imm/imm1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#=================================================
+# DaoDao's script
+#=================================================
+##添加自己的插件库
+               
+sed -i 's/git.openwrt.org\/feed/github.com\/openwrt/g' feeds.conf.default
+sed -i 's/git.openwrt.org\/project/github.com\/openwrt/g' feeds.conf.default
+
+#echo "src-git fichenx https://github.com/fichenx/openwrt-package" >> feeds.conf.default
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+#echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
